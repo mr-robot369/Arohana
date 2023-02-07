042d52177cf46ff25005fcfcdf5688356e5e1dcf
Anubhav <anubhavshukla870@gmail.com>
2023-02-07 11:06:50 +0530

    competitions

---
+++ Javascript/competitions.js
@@ -0,0 +1,10 @@
+function clickinner(target){
+    // let neww=document.getElementsByClassName('tilt-box-wrap');
+    // neww.onClick="location.href='index.html";
+    if(target=="dance"){
+        window.location.href='index.html';
+    }
+    else if (target=="music"){
+        window.location.href='about.html';
+    }
+}
